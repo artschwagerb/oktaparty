b6b1ced991b4efa6a42cf1a4e3451429a92713c2
Brian Artschwager <artschwagerb@users.noreply.github.com>
2022-02-14 10:39:20 -0500

    Your Groups, List Groups and proper redirects back (#6)

--- app/okta/urls.py
+++ app/okta/urls.py
@@ -6,6 +6,7 @@ app_name = 'okta'
 urlpatterns = [
     path('', views.index, name='index'),
     path('group/list', views.group_list, name='group-list'),
+    path('group/yours', views.group_yours, name='group-yours'),
     path('group/<int:group_id>/join', views.group_join, name='group-join'),
     path('group/<int:group_id>/leave', views.group_leave, name='group-leave'),
     #path('groups/sync', views.group_sync, name='group-sync'),
